0445d4c24708877d052c0e603ea90089bf284cb0
Hamid Ali Kalhoro <hamidKalhoro.in@gmail.com>
2025-06-06 12:51:26 +0500

    calling for downloading endpoint

--- src/main/java/com/hamidz/fm/FileManagerController.java
+++ src/main/java/com/hamidz/fm/FileManagerController.java
@@ -1,12 +1,19 @@
 package com.hamidz.fm;
 
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.logging.Level;
 
 @RestController
@@ -29,4 +36,20 @@ public class FileManagerController {
         }
         return false;
     }
+
+    @GetMapping("/download")
+    public ResponseEntity<InputStreamResource> downloadFile(@RequestParam("fileName") String fileName) {
+        try {
+            var fileToDownload = fileStorageService.getDownloadFile(fileName);
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                    .contentLength(fileToDownload.length())
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .body(new InputStreamResource(Files.newInputStream(fileToDownload.toPath())));
+        } catch (IOException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
 }
--- src/main/java/com/hamidz/fm/FileStorageService.java
+++ src/main/java/com/hamidz/fm/FileStorageService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
@@ -23,5 +24,19 @@ public class FileStorageService {
             throw new SecurityException("Unsupported filename!");
         }
         Files.copy(fileToSave.getInputStream(), targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    public File getDownloadFile(String fileName) throws FileNotFoundException {
+        if (fileName.isEmpty()) {
+            throw new IllegalArgumentException("File name cannot be null or empty");
+        }
+        var fileToDownload = new File(STORAGE_DIRECTORY + File.separator + fileName);
+        if (!Objects.equals(fileToDownload.getParent(), STORAGE_DIRECTORY)) {
+            throw new SecurityException("Unsupported filename!");
+        }
+        if (!fileToDownload.exists()) {
+            throw new FileNotFoundException("No File Named " + fileName + " found in storage");
+        }
+        return fileToDownload;
     }
 }
